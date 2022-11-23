f43154f1da63f120d2dc0e0360e24dc88674e9e6
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-11-23 20:26:25 +0100

    initial commit

---
+++ controllers/shop.js
@@ -0,0 +1,34 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Producrs',
+      path: '/products ',
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/cart',
+  });
+};
+
+exports.getCheckout = (res, req, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout Page',
+    path: '/checkout',
+  });
+};
