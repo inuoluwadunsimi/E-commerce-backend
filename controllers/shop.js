f736374130ba3dcb1823e4ca3a9db54506848f87
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-11-23 21:56:41 +0100

    created dynamic adding for add product

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,7 @@ exports.getProducts = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/index', {
+      prods:products,
       pageTitle: 'Shop',
       path: '/',
     });
--- routes/admin.js
+++ routes/admin.js
@@ -14,6 +14,6 @@ router.get('/add-product', adminController.getAddProduct );
 // /admin/add-product => POST
 router.post('/add-product',adminController.postAddProduct );
 
-router.get('/products')
+router.get('/products',adminController.getAdminProducts)
 
 module.exports = router;
