825dad0449093bf072e1453d86820d1019a3a3ca
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-12-04 18:00:55 +0100

    started working on postcart controller and added an includes or add to cart

--- models/product.js
+++ models/product.js
@@ -17,14 +17,15 @@ const getProductsFromFile = (cb) => {
   });
 };
 module.exports = class Product {
-  constructor(title,imageUrl,price,description,) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
-    this.imageUrl = imageUrl
-    this.price = price
-    this.description = description
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,4 +37,13 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
+
+  
 };
--- routes/shop.js
+++ routes/shop.js
@@ -7,11 +7,15 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get('/cart',shopController.getCart);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/checkout',shopController.getCheckout);
+router.get('/cart', shopController.getCart);
 
-router.get('/orders',shopController.getOrders)
+router.post('/cart',shopController.postCart)
+
+router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders);
 module.exports = router;
