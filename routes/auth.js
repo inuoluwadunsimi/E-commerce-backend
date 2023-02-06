7aebfeedfc62c9b0032dc4a4ea6a42191646a78b
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2023-02-06 12:06:44 +0100

    fix: fix confirm password bug

--- routes/auth.js
+++ routes/auth.js
@@ -47,14 +47,50 @@ router.post(
       .isLength({ min: 5 })
       .isAlphanumeric(),
     body('confirmPassword').custom((value, { req }) => {
-      if (value !== req.body.Password) {
+      if (value !== req.body.password) {
         throw new Error('Password have to match!');
       }
       return true;
-    }),
+    })
   ],
   authController.postSignup
 );
+
+// router.post(
+//     '/signup',
+//     [
+//       check('email')
+//         .isEmail()
+//         .withMessage('Please enter a valid email.')
+//         .custom((value, { req }) => {
+//           // if (value === 'test@test.com') {
+//           //   throw new Error('This email address if forbidden.');
+//           // }
+//           // return true;
+//           return User.findOne({ email: value }).then(userDoc => {
+//             if (userDoc) {
+//               return Promise.reject(
+//                 'E-Mail exists already, please pick a different one.'
+//               );
+//             }
+//           });
+//         }),
+//       body(
+//         'password',
+//         'Please enter a password with only numbers and text and at least 5 characters.'
+//       )
+//         .isLength({ min: 5 })
+//         .isAlphanumeric(),
+//       body('confirmPassword').custom((value, { req }) => {
+//         if (value !== req.body.password) {
+//           throw new Error('Passwords have to match!');
+//         }
+//         return true;
+//       })
+//     ],
+//     authController.postSignup
+//   );
+
 
 router.get('/reset', authController.getReset);
 
