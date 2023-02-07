c56f284de3aeb376df20ace54c38c1b2f22c3ef2
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2023-02-08 00:45:17 +0100

    chore: handle errors in sessions

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const sessionStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf')
-const flash = require('connect-flash')
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 require('dotenv').config();
 
@@ -18,7 +18,7 @@ const store = new sessionStore({
   collection: 'sessions',
 });
 
-const csrfProtection = csrf()
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -39,9 +39,8 @@ app.use(
   })
 );
 
-app.use(csrfProtection)
-app.use(flash())
-
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -49,17 +48,22 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      throw new Error(err);
+    });
 });
 
-app.use((req,res,next)=>{
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.csrfToken = req.csrfToken()
-  next()
-})
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
